3f38dc6b0a31fe9bacb3a455977442d7aef5f219
JesGor <jesusgorillo@gmail.com>
2015-11-14 21:06:50 +0100

    añadiendo setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='test_rest',
+	version='0.0.1',
+	description='Aplicación web muy simple con interfaz REST creada para un ejercicio de la asignatura IV',
+	url='https://github.com/JesGor/test_rest',
+	author='Jesus Prieto Lopez',
+	author_email='jesusgorillo@gmail.com',
+	license='GNU General Public License',
+	packages=['test_rest'],
+	zip_safe=False)
