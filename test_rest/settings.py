12e71de1be581e3a3930d88c630fabeedbfdb980
JesGor <jesusgorillo@gmail.com>
2015-11-14 20:15:48 +0100

    Añadiendo requirements y cambiando settings

--- test_rest/settings.py
+++ test_rest/settings.py
@@ -81,7 +81,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+ON_HEROKU = os.environ.get('PORT')
+if ON_HEROKU:
+    DATABASE_URL='postgres://xfkguxxjdcmgrp:cqk8UhZfqmBKystbg39wVZDWyB@ec2-54-204-6-113.compute-1.amazonaws.com:5432/d1ea7k9gsqu3j1'
+    DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
