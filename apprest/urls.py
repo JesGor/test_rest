e92d89ea389ef5f0998784518a7b51f93c3d079f
JesGor <jesusgorillo@gmail.com>
2015-11-14 20:11:45 +0100

    Arreglando url

--- apprest/urls.py
+++ apprest/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from apprest import views
 
 urlpatterns = [
-    url(r'^empresas/$', views.lista_empresas),
+    url(r'^$', views.lista_empresas),
     url(r'^empresas/(?P<pk>[0-9]+)/$', views.empresa),
 ]
